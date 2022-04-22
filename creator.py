0583319c735dbcce72f65d247829ef3c35b9057c
sick2as <103950270+sick2as@users.noreply.github.com>
2022-04-22 16:05:46 -0500

    Create creator.py

---
+++ creator.py
@@ -0,0 +1,10 @@
+import random
+num1,num2 = random.randint(1,9), random.randint(0,9)
+#next udate adds more than just addition but I just wanted to make this real simple
+'''
+#only use if using linux command line to run
+@mpine.lip():
+ asy nc def return
+  as root {path}
+'''
+print(f"the sum of {num1} and {num2} is {int(num1) + int(num2)}")
