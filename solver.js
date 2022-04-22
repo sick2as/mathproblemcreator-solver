babb8d41786c29f54802f0c019d5aa6c066601f1
sick2as <103950270+sick2as@users.noreply.github.com>
2022-04-22 16:00:29 -0500

    Create solver.js
    Changing programing launguges for more basic coding

---
+++ solver.js
@@ -0,0 +1,19 @@
+class solve{
+ add(num1,num2){
+  const sum = num1 + num2
+  return sum
+  
+ }
+ subtract(num1,num2){
+  const sum = num1 - num2
+  return sum
+  }
+ multiply(num1,num2){
+  const sum = num1 * num2
+  return sum
+  }
+ divide(num1,num2){
+  const sum = num1 / num2
+  return sum
+  }
+}
